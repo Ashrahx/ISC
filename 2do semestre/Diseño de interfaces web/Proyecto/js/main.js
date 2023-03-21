8c89811710210ada4df12db6b5035d6049ddc0c4
Ashrah <elfudanshi5@gmail.com>
2023-03-21 17:54:41 -0600

    Update

--- 2do semestre/Diseño de interfaces web/Proyecto/js/main.js
+++
@@ -1,7 +0,0 @@
-// Código JavaScript para activar el menú de hamburguesa
-const burger = document.querySelector('.burger');
-		const nav = document.querySelector('nav');
-
-		burger.addEventListener('click', () => {
-			nav.classList.toggle('active');
-		});
